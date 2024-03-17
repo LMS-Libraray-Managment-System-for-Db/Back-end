de5169fad9102c56fb66d7a1194e213e3d31a000
Engfathy <fathy01015485028@gmail.com>
2024-03-17 22:37:17 +0200

    connect to mysql database

--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotEnv from "dotenv";
 // import userRouter from "./router/userRouter";
-
+import { connectToDatabase } from './database/connectDb';
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 // import { defaultLimiter } from "./middleware/reqLimiter";
 import hpp from "hpp";
-
 import http from "http";
-
 
 
 const app: express.Application = express();
 const server = http.createServer(app);
-
-
-
+dotEnv.config({ path: "./../config.env" });
+const hostName: string | any = process.env.HOST_NAME || "0.0.0.0";
+const port: number = Number(process.env.PORT) || 5000;
+connectToDatabase();
 app.set("trust proxy", 0);
-
 // middleware
 app.use(
     cors({
@@ -49,10 +47,6 @@ app.use(hpp());
 // app.use("/api/v1/module", defaultLimiter, moduleRouter);
 // app.use("/api/v1/project", defaultLimiter, projectRouter);
 
-dotEnv.config({ path: "./../config.env" });
-const hostName: string | any = process.env.HOST_NAME || "0.0.0.0";
-
-const port: number = Number(process.env.PORT) || 5000;
 
 //-------------------------------------------------------
 
