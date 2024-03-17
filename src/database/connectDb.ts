a22e9fe7405d17a8f62e717b282bc151aedb29a9
Engfathy <fathy01015485028@gmail.com>
2024-03-17 22:38:32 +0200

    ..

---
+++ src/database/connectDb.ts
@@ -0,0 +1,23 @@
+
+import mysql from 'mysql2';
+
+let connection;
+
+export async function connectToDatabase() {
+ try {
+    connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'fathy123',
+      database: 'library_management',
+      port: 3306,
+    });
+
+    console.log('Connected to MySQL database');
+ } catch (err) {
+    console.error('Error connecting to MySQL database:', err);
+    throw err;
+ }
+}
+
+export { connection };
